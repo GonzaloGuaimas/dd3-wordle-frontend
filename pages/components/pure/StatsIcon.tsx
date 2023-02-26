04a75c23df009f4121dc6b5b67e9541f5deba9b8
gonzaloguaimas <gonzaloguaimas@gmail.com>
2023-02-26 17:50:40 -0300

    rename properties

--- pages/components/pure/StatsIcon.tsx
+++ pages/components/pure/StatsIcon.tsx
@@ -4,9 +4,9 @@ const StatsIcon = () => {
   return (
     <svg width="40" height="36" viewBox="0 0 40 36" fill="none" xmlns="http://www.w3.org/2000/svg">
         <rect x="4.93549" y="6" width="29.6129" height="24" rx="2" className='fill-grey-key dark:fill-white'/>
-        <path d="M13.1613 15L13.1613 24" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" className='stroke-white dark:stroke-stroke-dark'/>
-        <path d="M19.7419 18V24" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" className='stroke-white dark:stroke-stroke-dark'/>
-        <path d="M26.3226 12V24" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" className='stroke-white dark:stroke-stroke-dark'/>
+        <path d="M13.1613 15L13.1613 24" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" className='stroke-white dark:stroke-stroke-dark'/>
+        <path d="M19.7419 18V24" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" className='stroke-white dark:stroke-stroke-dark'/>
+        <path d="M26.3226 12V24" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" className='stroke-white dark:stroke-stroke-dark'/>
     </svg>
   )
 }
